f60c4ad9491b7990c5d0c5a782c062dbead80401
tanghea <tanghea@yonyou.com>
2017-10-13 17:02:47 +0800

    行业类别

--- ma-contract-domain/src/main/java/com/yonyou/energy/contract/domain/IndustryCategory.java
+++ ma-contract-domain/src/main/java/com/yonyou/energy/contract/domain/IndustryCategory.java
@@ -116,4 +116,61 @@ public class IndustryCategory implements Serializable {
     public void setTs(String ts) {
         this.ts = ts;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        IndustryCategory that = (IndustryCategory) o;
+
+        if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+        if (getCode() != null ? !getCode().equals(that.getCode()) : that.getCode() != null) return false;
+        if (getName() != null ? !getName().equals(that.getName()) : that.getName() != null) return false;
+        if (getStatus() != null ? !getStatus().equals(that.getStatus()) : that.getStatus() != null) return false;
+        if (getReason() != null ? !getReason().equals(that.getReason()) : that.getReason() != null) return false;
+        if (getCreator() != null ? !getCreator().equals(that.getCreator()) : that.getCreator() != null) return false;
+        if (getCreationtime() != null ? !getCreationtime().equals(that.getCreationtime()) : that.getCreationtime() != null)
+            return false;
+        if (getModifier() != null ? !getModifier().equals(that.getModifier()) : that.getModifier() != null)
+            return false;
+        if (getModifytime() != null ? !getModifytime().equals(that.getModifytime()) : that.getModifytime() != null)
+            return false;
+        if (getDr() != null ? !getDr().equals(that.getDr()) : that.getDr() != null) return false;
+        return getTs() != null ? getTs().equals(that.getTs()) : that.getTs() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getCode() != null ? getCode().hashCode() : 0);
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getStatus() != null ? getStatus().hashCode() : 0);
+        result = 31 * result + (getReason() != null ? getReason().hashCode() : 0);
+        result = 31 * result + (getCreator() != null ? getCreator().hashCode() : 0);
+        result = 31 * result + (getCreationtime() != null ? getCreationtime().hashCode() : 0);
+        result = 31 * result + (getModifier() != null ? getModifier().hashCode() : 0);
+        result = 31 * result + (getModifytime() != null ? getModifytime().hashCode() : 0);
+        result = 31 * result + (getDr() != null ? getDr().hashCode() : 0);
+        result = 31 * result + (getTs() != null ? getTs().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "IndustryCategory{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                ", reason='" + reason + '\'' +
+                ", creator=" + creator +
+                ", creationtime='" + creationtime + '\'' +
+                ", modifier=" + modifier +
+                ", modifytime='" + modifytime + '\'' +
+                ", dr=" + dr +
+                ", ts='" + ts + '\'' +
+                '}';
+    }
 }
