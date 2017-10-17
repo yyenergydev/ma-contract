e8d42f718b1679b78d2c651fbaa29dd4432267bf
tanghea <tanghea@yonyou.com>
2017-10-17 15:37:36 +0800

    采购合同

--- ma-contract-api/src/main/java/com/yonyou/energy/contract/api/IContractPuService.java
+++ ma-contract-api/src/main/java/com/yonyou/energy/contract/api/IContractPuService.java
@@ -1,7 +1,46 @@
 package com.yonyou.energy.contract.api;
 
+import com.yonyou.energy.common.domain.criteria.BaseCriteria;
+import com.yonyou.energy.common.domain.response.ServiceResponse;
+import com.yonyou.energy.contract.domain.ContractPu;
+
 /**
  * Created by tanghe on 2017/10/17.
  */
 public interface IContractPuService {
+
+    /**
+     * 列表查询
+     * @param criteria
+     * @return
+     */
+    public ServiceResponse<String> list(BaseCriteria criteria);
+
+    /**
+     * 通过pk查询
+     * @param criteria
+     * @return
+     */
+    public ServiceResponse<String> selectByPk(BaseCriteria criteria);
+
+    /**
+     * 暂存（新增保存，修改保存）
+     * @param data
+     * @return
+     */
+    public ServiceResponse<String> tempsave(ContractPu data, BaseCriteria criteria);
+
+    /**
+     * 提交
+     * @param data
+     * @return
+     */
+    public ServiceResponse<String> save(ContractPu data, BaseCriteria criteria);
+
+    /**
+     * 删除
+     * @param criteria
+     * @return
+     */
+    public ServiceResponse<String>  delete(BaseCriteria criteria);
 }
